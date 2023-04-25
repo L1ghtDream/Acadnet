86189947093a14e01bf6395895c0750f58c0ac92
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-25 19:33:28 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
-#include <bits/stdc++.h>
+#include<stdio.h>
 
-// You can't use any of the following words: for, while, return, begin, end, goto.		// You can NOT edit this
-auto sum(const std::vector<int> &v, const int left, const int right) {					// You can edit this
-    if (left == right) {																// You can edit this
-        return v[left]; 																// You can edit this
-    } else {																			// You can edit this
-        auto mid = (left+right)	/2		;								// You can edit this
-        int res = sum(v, left, mid) + sum(v, mid + 1, right);				// You can edit ths
-        return res;																		// You can NOT edit this
+int a[100][100], b[100][100], c[100][100];
+
+int i, j, k, m, n, p;
+
+int main()
+{
+    scanf("%d%d%d", &n, &m, &p);
+
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < m; ++j) {
+            scanf("%d", &a[i][j]);
+        }
     }
-}
 
-int main() {
-    int n;                                                  							// You can NOT edit this
-    std::vector<int> v;																	// You can NOT edit this
-    std::cin >> n;																		// You can NOT edit this
-    v.resize(n); 																		// You can NOT edit this
-    // You can NOT edit this
-    for (int i = 0; i < n; i++) {														// You can NOT edit this
-        std::cin >> v[i];																// You can NOT edit this
-    }																					// You can NOT edit this
-    std::cout << sum(v, 0, v.size() - 1) << "\n";										// You can NOT edit this
-    return 0;																			// You can NOT edit this
+    for (i = 0; i < m; ++i) {
+        for(j = 0; j < p; ++j) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < p; j++) {
+            for (k = 0; k < m; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < p; ++j) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
 }
