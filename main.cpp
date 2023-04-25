1c4deb85055fa06c91a99b748640003f605dea89
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-25 19:31:01 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,100 +1,25 @@
-#include <cstdio>
-#include <queue>
+#include <bits/stdc++.h>
 
-#define MAX_N 1000
-
-using namespace std;
-
-struct point {
-    int l, c;
-};
-
-// there is a path available on that square
-bool path[MAX_N][MAX_N];
-// we have seen that squre
-bool seen[MAX_N][MAX_N];
-// we can reach that wall
-bool reachable_wall[MAX_N][MAX_N];
-
-
-
-point dir[] = {
-        {0,1},
-        {0,-1},
-        {1,0},
-        {-1,0},
-        {1,1},
-        {1,-1},
-        {-1,1},
-        {-1,-1}
-};
-
-// Perform a BFS from coordinates (startX, startY)
-bool bfs(int startX, int startY, int targetX, int targetY, bool touch_wall) {
-    int i;
-    queue<point> q;
-
-    seen[startX][startY] = true;
-    q.push({startX, startY});
-    while (!q.empty()) {
-        point p = q.front();
-        q.pop();
-
-        for (i = 0; i < (int) (sizeof(dir) / sizeof(point)); i++) {
-            point new_p = {p.l + dir[i].l, p.c + dir[i].c};
-
-            if(new_p.l==targetX && new_p.c==targetY){
-                return true;
-            }
-
-            if (!path[new_p.l][new_p.c] && touch_wall) {
-                reachable_wall[new_p.l][new_p.c] = true;
-            } else if (reachable_wall[new_p.l][new_p.c] && !touch_wall) {
-                return true;
-            } else if (!seen[new_p.l][new_p.c] && path[new_p.l][new_p.c]) {
-                seen[new_p.l][new_p.c] = true;
-
-                q.push(new_p);
-            }
-        }
+// You can't use any of the following words: for, while, return, begin, end, goto.		// You can NOT edit this
+auto sum(const std::vector<int> &v, const int left, const int right) {					// You can edit this
+    if (left == right) {																// You can edit this
+        return v[left]; 																// You can edit this
+    } else {																			// You can edit this
+        auto mid = (left+right)	/2		;								// You can edit this
+        int res = sum(v, left, mid) + sum(v, mid + 1, right);				// You can edit ths
+        return res;																		// You can NOT edit this
     }
-
-    return false;
 }
 
-
-int main(void) {
-    int L1, C1, L2, C2;
-
-    int N, M;
-    int i, j;
-
-    // read the input
-    scanf("%d %d", &N, &M);
-    for (i = 1; i <= N; ++i) {
-        for (j = 1; j <= M; ++j) {
-            int cell;
-
-            scanf("%d", &cell);
-            // set the path availability
-            path[i][j] = cell == 0;
-        }
-    }
-
-    scanf("%d %d %d %d", &L1, &C1, &L2, &C2);
-
-    //bfs(L1, C1, true);
-
-    // if the first player can reach the second one
-    if (bfs(L1, C1,L2,C2, true)) {
-        printf("OK.\n");
-        return 0;
-    }
-
-    if (bfs(L2, C2,L1,C1, false))
-        printf("BOOM.\n");
-    else
-        printf("!OK.\n");
-
-    return 0;
+int main() {
+    int n;                                                  							// You can NOT edit this
+    std::vector<int> v;																	// You can NOT edit this
+    std::cin >> n;																		// You can NOT edit this
+    v.resize(n); 																		// You can NOT edit this
+    // You can NOT edit this
+    for (int i = 0; i < n; i++) {														// You can NOT edit this
+        std::cin >> v[i];																// You can NOT edit this
+    }																					// You can NOT edit this
+    std::cout << sum(v, 0, v.size() - 1) << "\n";										// You can NOT edit this
+    return 0;																			// You can NOT edit this
 }
