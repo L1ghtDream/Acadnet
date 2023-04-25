77c46b0defa8880bb0fc0e8765df23716d60a738
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-25 19:43:16 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include<stdio.h>
 
-std::string parse_10digit_number(std::string number) {
-    return number.substr(0, 4) + " " + number.substr(4, 3) + " " + number.substr(7, 3);
-}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int m = n * (n + 1) / 2;
 
+    int sum = 0;
+    for (int i = 0; i <= n; ++i)
+        sum += i;
 
-std::string parse_phone_number(std::string number) {
-    //cout<<number.size()<<endl;
-    switch(number.size()) {
-        case 10:
-            return "(004) " + parse_10digit_number(number);
-        case 12:
-            return "(004) " + parse_10digit_number(number.substr(2, number.size() - 2));
-        case 13:
-            return "(004) " + parse_10digit_number(number.substr(2, number.size() - 3));
-        default:
-            return "";
-    }
-}
-
-
-int main() {
-    int size;
-    std::string phone_number;
-    cin>>size;
-    //cin.get();
-
-    for (int i = 0; i < size; i++) {
-        //getline(cin, phone_number);
-        std::cin >> phone_number;
-        std::cout << parse_phone_number(phone_number) << std::endl;
-    }
+    printf("Suma primelor %d numere este %d = %d\n", n, m, sum);
 }
