6ade4465a73142fe8359f4686e40d0007c5dbf13
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-25 19:26:38 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,64 +1,100 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <queue>
 
-/* -------------------------- Defining a set -------------------------------- */
+#define MAX_N 1000
 
-#define MAX_NR 10000000
-#define SET_SIZE ((MAX_NR + 1) / 8)
-typedef unsigned char SET[SET_SIZE];
+using namespace std;
 
-SET my_set = {0};
+struct point {
+    int l, c;
+};
 
-/* ------------------------- Helper functions! ------------------------------ */
-
-static inline void insert_in_set(SET s, unsigned int n) {
-    // Find the corresponding cell and set one of its bits
-    s[n / 8] |= 1 << n % 8;
-}
-
-static inline int is_in_set(SET s, unsigned int n) {
-    // Find the corresponding cell and test one of its bits
-    return (s[n / 8] & (1 << n % 8)) > 0;
-}
-
-/* ------------------------ Sieve functions! -------------------------------- */
-
-unsigned int find_next_prime(SET sieve, unsigned int x) {
-    while (is_in_set(sieve, ++x));
-
-    return x;
-}
-
-void init_seive(SET sieve) {
-    unsigned int i, d;
-
-    insert_in_set(sieve, 0);
+// there is a path available on that square
+bool path[MAX_N][MAX_N];
+// we have seen that squre
+bool seen[MAX_N][MAX_N];
+// we can reach that wall
+bool reachable_wall[MAX_N][MAX_N];
 
 
-    for (d = 2; d <= MAX_NR; d++) {
-        if (!is_in_set(sieve, d)) {              // if d is prime
-            for (i = d * d; i <= MAX_NR; i += d) { // we'll jump d steps and mark
-                insert_in_set(sieve, i);
+
+point dir[] = {
+        {0,1},
+        {0,-1},
+        {1,0},
+        {-1,0},
+        {1,1},
+        {1,-1},
+        {-1,1},
+        {-1,-1}
+};
+
+// Perform a BFS from coordinates (startX, startY)
+bool bfs(int startX, int startY, int targetX, int targetY, bool touch_wall) {
+    int i;
+    queue<point> q;
+
+    seen[startX][startY] = true;
+    q.push({startX, startY});
+    while (!q.empty()) {
+        point p = q.front();
+        q.pop();
+
+        for (i = 0; i < (int) (sizeof(dir) / sizeof(point)); i++) {
+            point new_p = {p.l + dir[i].l, p.c + dir[i].c};
+
+            if(new_p.l==targetX && new_p.c==targetY){
+                return true;
+            }
+
+            if (!path[new_p.l][new_p.c] && touch_wall) {
+                reachable_wall[new_p.l][new_p.c] = true;
+            } else if (reachable_wall[new_p.l][new_p.c] && !touch_wall) {
+                return true;
+            } else if (!seen[new_p.l][new_p.c] && path[new_p.l][new_p.c]) {
+                seen[new_p.l][new_p.c] = true;
+
+                q.push(new_p);
             }
         }
     }
+
+    return false;
 }
 
 
-int main() {
-    unsigned int n, x;
+int main(void) {
+    int L1, C1, L2, C2;
 
-    init_seive(my_set);
+    int N, M;
+    int i, j;
 
-    scanf("%u", &n);
-    while (n--) {
-        scanf("%u", &x);
-        printf("%u\n", find_next_prime(my_set, x));
+    // read the input
+    scanf("%d %d", &N, &M);
+    for (i = 1; i <= N; ++i) {
+        for (j = 1; j <= M; ++j) {
+            int cell;
+
+            scanf("%d", &cell);
+            // set the path availability
+            path[i][j] = cell == 0;
+        }
     }
+
+    scanf("%d %d %d %d", &L1, &C1, &L2, &C2);
+
+    //bfs(L1, C1, true);
+
+    // if the first player can reach the second one
+    if (bfs(L1, C1,L2,C2, true)) {
+        printf("OK.\n");
+        return 0;
+    }
+
+    if (bfs(L2, C2,L1,C1, false))
+        printf("BOOM.\n");
+    else
+        printf("!OK.\n");
 
     return 0;
 }
-
-// 5 1 2009 10 7 92
