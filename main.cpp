6883b3508592b2f3c44ff82fd7a18f11a39e62cb
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 09:30:08 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    unsigned long long a, b;
-    cin >> a >> b;
-    if (b > INT_MAX / a) { // Poti modifica doar aceasta linie!
-        cout << "Nu";
+double find_best_phone(std::vector<int> &phones) {
+    sort(phones.begin(), phones.end());
+    int number_phones = phones.size();
+    if (number_phones % 2 == 1) {
+        return phones[number_phones / 2];
     } else {
-        cout << "Da";
+        return (phones[number_phones / 2 - 1] + phones[number_phones / 2]) / 2.0;
     }
+}
+
+
+int main() {
+    int n;
+    ifstream fin("../input.txt");
+    fin >> n;
+    vector<int> phones(n,0);
+    for (int i = 0; i < n; ++i) {
+        fin >> phones[i];
+    }
+
+    ofstream fout("../output.txt");
+    fout << find_best_phone(phones) << "\n";
+    fin.close();
+    fout.close();
     return 0;
 }
