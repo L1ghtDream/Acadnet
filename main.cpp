e3dc6f341bd37423da43e6840af841324e4a3bf0
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 12:04:13 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,53 +1,29 @@
 #include <iostream>
-#include <vector>
+using namespace std;
 
-#define POLI_FORBIDDEN_DREAM 6969696969
+bool is_prime(int x) {
+    for (int i = 2; i*i <= x; i++)
+        if (x % i == 0)
+            return false;
 
-using zacusca_t = std::vector<std::vector<int>>;
-using strangers_t = std::vector<long long>;
+    return true;
+}
 
-
-int main() {
-    int n;
-    std::cin >> n;
-
-    zacusca_t zacusca(n + 1, std::vector<int>(n + 1, 0));
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            std::cin >> zacusca[i][j];
-        }
-    }
-
-    strangers_t strangers(n + 1, 0);
-    for (int i = 1; i <= n; ++i) {
-        std::cin >> strangers[i];
-    }
-
-    std::vector<long long int> dp(n + 1);
-    // Caz de baza
-    dp[0] = 0;
-
-    // Caz general
-    for (int i = 1; i <= n; ++i) {
-        // Adaugam interactiunile cu strainii.
-        dp[i] = strangers[i];
-
-        // Adaugam interactiunile de tip zacusca.
-        for (int j = 1; j < i; ++j) {
-            if (zacusca[j][i]) {
-                dp[i] = (dp[i] + dp[j]) % POLI_FORBIDDEN_DREAM;
+class SubMain{
+public:
+    SubMain(){
+        int n;
+        cin>>n;
+        for(int i=2;i<n;i++){
+            if(is_prime(i)){
+                cout<<i<<endl;
             }
         }
     }
+};
 
-    // Solutie: X = sum(dp[i])
-    long long int X = 0;
-    for (int i = 1; i <= n; ++i) {
-        X = (X + dp[i]) % POLI_FORBIDDEN_DREAM;
-    }
+SubMain subMain = SubMain();
 
-    std::cout << X << std::endl;
 
-    return 0;
-}
 
+int main() { return 0; }
