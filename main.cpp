71d433a1c90a6163bfabc19f198cbb91496be7be
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 12:10:29 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,45 +1,17 @@
 #include <iostream>
-
-#define N 2586134
-
-unsigned int _ = 0;
-
-int reference(int a);
-
-void add(int __) {
-    _ += __;
-}
-
-void add_again(int __) {
-    _ += __ + 2;
-}
-
-void add_once_again(int __) {
-    _ += __ * 3 + 2;
-}
-
-void compute_number(int mySweetVar) {
-    if (mySweetVar % 2 == 0) {
-        for (int i = N; i >= 0; i--) {
-            add(i);
-        }
-    } else if (mySweetVar % 3 == 0) {
-        for (int i = N; i >= 0; i--) {
-            add_again(i);
-        }
-    } else {
-        for (int i = N; i >= 0; i--) {
-            add_once_again(i);
-        }
-    }
-
-}
+#include <stdlib.h>
+using namespace std;
 
 int main(void) {
-    int mySweetVar;
-    std::cin >> mySweetVar;
-    _ = mySweetVar;
-    compute_number(mySweetVar);
-    std::cout << _ << "\n";
+    int a,b = 0;
+    srand(66);
+    a = rand();                     // This is
+    b = rand();                     // "pure luck"
+    if ((a == b) && (a-1 == b-1))
+    {
+        std::cout << "fail\n";
+        return 0;
+    }
+    std::cout << "success\n";
+    return 0;
 }
-
