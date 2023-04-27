33066eb4c352178e60269cadcee1e1bc1fcb8bad
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 09:54:32 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
-#include <bits/stdc++.h>                                // You cannot change this line!
-// You cannot change this line!
-int main() {                                            // You cannot change this line!
-    int n;                                              // You cannot change this line!
-    std::cin >> n;                                      // You cannot change this line!
-    // You cannot change this line!
-    std::vector<unsigned int> gpns;                     // You cannot change this line!
-    for (int i = 1; i <= n; ++i) {                      // You cannot change this line!
-        unsigned int gpn;                               // You cannot change this line!
-        std::cin >> gpn;                                // You cannot change this line!
-        gpns.push_back(gpn);                            // You cannot change this line!
-    }                                                   // You cannot change this line!
-    // You cannot change this line!
-    // You cannot change this line!
-    auto max_gpn = -1;                                  // You cannot change this line!
-    for (auto gpn : gpns) {                             // You cannot change this line!
-        if ((int)gpn > (int)max_gpn) {                            // You can edit this line for a beer!
-            max_gpn = gpn;                              // You can edit this line for a beer!
-        }                                               // You can edit this line for a beer!
-    }                                                   // You cannot change this line!
-    std::cout << max_gpn << "\n";                       // You cannot change this line!
-    return 0;                                           // You cannot change this line!
-}                                                       // You cannot change this line!
+#include <iostream>
+#include <iomanip>
+
+
+const double PI = 3.14159265;
+
+int main() {
+    int n, a, b;
+
+    // this is fine
+    std::cout << std::fixed;
+
+    std::cin >> n;
+    for (int i = 0; i < n; i++){
+        std::cin >> a >> b;
+        std::cout << PI * a * b << std::endl;
+    }
+
+
+    return 0;
+}
