6ab132df9088c28e47fbd0f9b60cd2bb93ff4c07
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 12:07:27 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
 #include <iostream>
-using namespace std;
 
-bool is_prime(int x) {
-    for (int i = 2; i*i <= x; i++)
-        if (x % i == 0)
-            return false;
+#define N 2586134
 
-    return true;
+unsigned int _ = 0;
+
+int reference(int a);
+
+void add(int __) {
+    _ += __;
 }
 
-class SubMain{
-public:
-    SubMain(){
-        int n;
-        cin>>n;
-        for(int i=2;i<n;i++){
-            if(is_prime(i)){
-                cout<<i<<endl;
-            }
+void add_again(int __) {
+    _ += __ + 2;
+}
+
+void add_once_again(int __) {
+    _ += __ * 3 + 2;
+}
+
+void compute_number(int mySweetVar) {
+    if (mySweetVar % 2 == 0) {
+        for (int i = N; i >= 0; i--) {
+            add(i);
+        }
+    } else if (mySweetVar % 3 == 0) {
+        for (int i = N; i >= 0; i--) {
+            add_again(i);
+        }
+    } else {
+        for (int i = N; i >= 0; i--) {
+            add_once_again(i);
         }
     }
-};
 
-SubMain subMain = SubMain();
+}
 
+int main(void) {
+    int mySweetVar;
+    std::cin >> mySweetVar;
+    _ = mySweetVar;
+    compute_number(mySweetVar);
+    std::cout << _ << "\n";
+}
 
-
-int main() { return 0; }
