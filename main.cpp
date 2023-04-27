272cb3b070e016ee89b7b6362e85f94db40f8cc5
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 10:03:06 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+using namespace std;
 
+class Statistics {
+    std::vector<int> numbers;
 
-std::vector<int> prime_factors(int n);
+public:
+    Statistics() {
+        numbers.clear();
+        int tmp;
 
-
-int main() {
-    int n;
-
-    std::cin >> n;
-    auto factors = prime_factors(n);
-
-    std::cout << n*factors[factors.size()-1] << std::endl;
-}
-
-
-std::vector<int> prime_factors(int n) {
-    std::vector<int> factors;
-
-    for (int i = 2; i <= n; i++) {
-        while (n % i == 0) {
-            factors.push_back(i);
-            n /= i;
+        int n;
+        cin>>n;
+        //while (std::cin >> tmp) {
+        for(int i=0;i<n;i++){
+            cin>>tmp;
+            numbers.push_back(tmp);
         }
     }
 
-    return factors;
+    int sum() {
+        int s = 0;
+
+        for (auto n: numbers)
+            s += n;
+
+        return s;
+    }
+
+    float median() {
+        const auto middle_itr = numbers.begin() + numbers.size() / 2;
+        std::nth_element(numbers.begin(), middle_itr, numbers.end());
+
+        if (numbers.size() % 2 == 0) {
+            const auto left_mid_itr = std::max_element(numbers.begin(), middle_itr);
+            return (*left_mid_itr + *middle_itr) / 2;
+        } else {
+            return *middle_itr;
+        }
+    }
+
+    float mean() {
+        return (float)sum() / numbers.size();
+    }
+};
+
+
+int main() {
+    Statistics stats;
+
+    std::cout << stats.sum()
+              << " " << stats.median()
+              << " " << stats.mean() << std::endl;
+
+    return 0;
 }
