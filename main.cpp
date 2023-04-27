a2fa30d418644d32b5fe14af1e465a55135b9668
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-27 09:51:47 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,97 +1,23 @@
-#include <fstream>
-#include <sstream>
-#include <iostream>
-
-using namespace std;
-
-/// Clasa memoreaza un nr complex
-class complex {
-private:
-    int r, i;   // r - partea reala, i - partea imaginara
-public:
-    complex();  // creeaza numarul imaginar nul
-    complex(int pr, int pi); // pr - valoarea termenului real, pi - val. term. imaginar
-    string afisare(); // pregateste un sir pentru a fi afisat
-    int real();     // returneaza partea reala
-    int imaginar(); // returneaza partea imaginara
-    void set(int pr, int pi); // stabileste valoarea partii reale si a celei imaginare
-};
-// aici va fi scris codul tau
-
-complex::complex() {
-    this->r = 0;
-    this->i = 0;
-}
-
-complex::complex(int pr, int pi) {
-    this->r = pr;
-    this->i = pi;
-}
-
-string complex::afisare() {
-    string output = "";
-
-    if(this->r!=0) {
-        output += std::to_string(this->r);
-    }
-
-    if(this->r!=0 && this->i>0) {
-        output += "+";
-    }
-
-    if(this->i!=0){
-        if(this->i!=1){
-            output += std::to_string(this->i);
-        }
-        output += "i";
-    }
-
-    return output;
-}
-
-int complex::real() {
-    return this->r;
-}
-
-int complex::imaginar() {
-    return this->i;
-}
-
-void complex::set(int pr, int pi) {
-    this->r = pr;
-    this->i = pi;
-}
-
-complex operator+(complex c1, complex c2){
-    return complex(c1.real()+c2.real(), c1.imaginar()+c2.imaginar());
-}
-
-complex operator-(complex c1, complex c2){
-    return complex(c1.real()-c2.real(), c1.imaginar()-c2.imaginar());
-}
-
-complex operator*(complex c1, complex c2){
-    // (a+b)(c+d)
-    // ac + ad + bc + bd
-
-    return complex(
-            c1.real()*c2.real() - c2.imaginar()*c1.imaginar(),
-            c1.real()*c2.imaginar() + c1.imaginar()*c2.real()
-    );
-}
-
-int main() {
-    fstream fin("../date.in", ios::in);
-    ofstream fout("../date.out");
-    int a, b, c, d;
-    while (fin >> a >> b >> c >> d) {
-        complex x(a, b), y(c, d), z;
-        z = x + y;
-        fout << z.afisare() << "\n";
-        z = x - y;
-        fout << z.afisare() << "\n";
-        z = x * y;
-        fout << z.afisare() << "\n";
-    }
-    return 0;
-}
+#include <bits/stdc++.h>                                // You cannot change this line!
+// You cannot change this line!
+int main() {                                            // You cannot change this line!
+    int n;                                              // You cannot change this line!
+    std::cin >> n;                                      // You cannot change this line!
+    // You cannot change this line!
+    std::vector<unsigned int> gpns;                     // You cannot change this line!
+    for (int i = 1; i <= n; ++i) {                      // You cannot change this line!
+        unsigned int gpn;                               // You cannot change this line!
+        std::cin >> gpn;                                // You cannot change this line!
+        gpns.push_back(gpn);                            // You cannot change this line!
+    }                                                   // You cannot change this line!
+    // You cannot change this line!
+    // You cannot change this line!
+    auto max_gpn = -1;                                  // You cannot change this line!
+    for (auto gpn : gpns) {                             // You cannot change this line!
+        if ((int)gpn > (int)max_gpn) {                            // You can edit this line for a beer!
+            max_gpn = gpn;                              // You can edit this line for a beer!
+        }                                               // You can edit this line for a beer!
+    }                                                   // You cannot change this line!
+    std::cout << max_gpn << "\n";                       // You cannot change this line!
+    return 0;                                           // You cannot change this line!
+}                                                       // You cannot change this line!
