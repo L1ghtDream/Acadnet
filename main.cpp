3e3547a880ba0e5c2175ae715fd9df2a9d569748
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-29 17:17:42 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,64 +1,24 @@
-#include <cstdio>
-
+#include <iostream>
 using namespace std;
+class A {
+public:
+    int s = 5;
 
-const int BUFSIZE = 160;
-
-char buf[BUFSIZE];
-int pbeg, len;
-FILE *fin = stdin;
-FILE *fout = stdout;
-
-int f() {
-    static int state = 0;
-    int ch;
-    int i;
-    int lastspc;
-
-    switch (state) {
-        case 0:
-            while (1) {
-                // AVETI VOIE SA MODIFICATI DE AICI
-                ch = fgetc(fin);
-
-                if (ch == EOF || ch == '\n') {
-                    break;
-                } else {
-                    buf[len++] = (char) ch;
-                }
-
-                if (ch == ' ') {
-                    if (len > 81) {
-                        for (i = 0; i < lastspc; i++) {
-                            fputc(buf[i], fout);
-                        }
-                        fputc('\n', fout);
-                        int index = 0;
-                        for (int j = lastspc; j <= len; j++, index++) {
-                            buf[index] = buf[j];
-                        }
-                        for (int j = index; j <= len; j++) {
-                            buf[j] = ' ';
-                        }
-                        len -= lastspc;
-                    }
-                    lastspc = len;
-                }
-            }
-            state = 1;
-            break;
-        case 1:        // NU AVETI VOIE SA STERGETI
-        case 2:        // NU AVETI VOIE SA STERGETI
-        case 4:        // NU AVETI VOIE SA STERGETI
-        case 3:        // NU AVETI VOIE SA STERGETI
-            break;
+    static void print1() {
+        cout << "1 ";
     }
-    return state;
-    // AVETI VOIE SA MODIFICATI PANA AICI
-}
 
+    void print2() {
+        std::cout << s;
+    }
+};
+
+A * a;
 int main() {
-    while (!f());
-
+    int i, s[] = {2, 3, 4};
+    a->print1();
+    for(i = 0; i < 3; i++)
+        cout << i+2 << " "; A b = A();a=&b;
+    a->print2();
     return 0;
 }
