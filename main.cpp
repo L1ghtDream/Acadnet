ee7898ff37638228c4ab0a893b50512b8a4c2324
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-29 17:11:09 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,47 +1,64 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <map>
+#include <cstdio>
 
 using namespace std;
 
+const int BUFSIZE = 160;
+
+char buf[BUFSIZE];
+int pbeg, len;
+FILE *fin = stdin;
+FILE *fout = stdout;
+
+int f() {
+    static int state = 0;
+    int ch;
+    int i;
+    int lastspc;
+
+    switch (state) {
+        case 0:
+            while (1) {
+                // AVETI VOIE SA MODIFICATI DE AICI
+                ch = fgetc(fin);
+
+                if (ch == EOF || ch == '\n') {
+                    break;
+                } else {
+                    buf[len++] = (char) ch;
+                }
+
+                if (ch == ' ') {
+                    if (len > 81) {
+                        for (i = 0; i < lastspc; i++) {
+                            fputc(buf[i], fout);
+                        }
+                        fputc('\n', fout);
+                        int index = 0;
+                        for (int j = lastspc; j <= len; j++, index++) {
+                            buf[index] = buf[j];
+                        }
+                        for (int j = index; j <= len; j++) {
+                            buf[j] = ' ';
+                        }
+                        len -= lastspc;
+                    }
+                    lastspc = len;
+                }
+            }
+            state = 1;
+            break;
+        case 1:        // NU AVETI VOIE SA STERGETI
+        case 2:        // NU AVETI VOIE SA STERGETI
+        case 4:        // NU AVETI VOIE SA STERGETI
+        case 3:        // NU AVETI VOIE SA STERGETI
+            break;
+    }
+    return state;
+    // AVETI VOIE SA MODIFICATI PANA AICI
+}
+
 int main() {
-    int n;
-    string target_language;
-    map<string, string> language_mapping;
-    string phrase;
-
-    cin >> n >> target_language;
-    for (int i = 1; i <= n; i++) {
-        string keyword, language, replaced_with;
-        cin >> keyword >> language >> replaced_with;
-        if (language != target_language)
-            continue;
-
-        language_mapping[keyword] = replaced_with;
-    }
-
-    getline(cin, phrase);
-    getline(cin, phrase);
-
-    string word;
-    stringstream phrase_stream(phrase);
-    vector<string> words;
-    while (getline(phrase_stream, word, ' '))
-        words.push_back(word);
-
-    for (int i = 0; i < int(words.size()); i++)
-        if (language_mapping.count(words[i]))
-            words[i] = language_mapping[words[i]];
-
-    for (int i = 0; i < int(words.size()); i++) {
-        cout << words[i];
-        if (i + 1 != int(words.size()))
-            cout << " ";
-        else
-            cout << "\n";
-    }
+    while (!f());
 
     return 0;
 }
