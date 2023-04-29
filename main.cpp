331f829944a83e0910cd7c425d5ab1f542e685c6
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-29 03:45:49 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-using namespace std;int main(){string s[1000];string a,b,c;while(cin>>a>>b>>c)if(a=="get"){int i=stoi(b);if(s[i].substr(0,s[i].find(';'))==c)cout<<s[i].substr(s[i].find(';')+1,s[i].size())+"\n";}else{int i=stoi(a);s[i]=b+";"+c;}}
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    string target_language;
+    map<string, string> language_mapping;
+    string phrase;
+
+    cin >> n >> target_language;
+    for (int i = 1; i <= n; i++) {
+        string keyword, language, replaced_with;
+        cin >> keyword >> language >> replaced_with;
+        if (language != target_language)
+            continue;
+
+        language_mapping[keyword] = replaced_with;
+    }
+
+    getline(cin, phrase);
+    getline(cin, phrase);
+
+    string word;
+    stringstream phrase_stream(phrase);
+    vector<string> words;
+    while (getline(phrase_stream, word, ' '))
+        words.push_back(word);
+
+    for (int i = 0; i < int(words.size()); i++)
+        if (language_mapping.count(words[i]))
+            words[i] = language_mapping[words[i]];
+
+    for (int i = 0; i < int(words.size()); i++) {
+        cout << words[i];
+        if (i + 1 != int(words.size()))
+            cout << " ";
+        else
+            cout << "\n";
+    }
+
+    return 0;
+}
