45760f06fec47492da85c7d2f9fe905196d2e34e
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-29 03:30:06 +0300

    (Semi) Finished

--- main.cpp
+++ main.cpp
@@ -1,106 +1,21 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 
-using namespace std;
-
-ifstream fin("../soarece.in");
-ofstream fout;
-
-int a[14][14], is, js, ib, jb, n, m;
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {-1, 0, 1, 0};
-vector<vector<vector<int>>> solutions;
-
-void citire() {
-    fin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++)
-            fin >> a[i][j];
-    }
-    fin >> is >> js >> ib >> jb;
-}
-
-void afis(bool final) {
-    if (!final) {
-        vector<vector<int>> solution;
-        for (int i = 1; i <= n; i++) {
-            vector<int> line;
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] == 1 || a[i][j] == 0)
-                    line.push_back(0);
-                else
-                    line.push_back(a[i][j]);
-            }
-
-            solution.push_back(line);
-        }
-
-        solutions.push_back(solution);
-        return;
-    }
-
-    int index = 0;
-    int maxxx = 0;
-    for (int i = 0; i < solutions.size(); i++) {
-        int maxx = 0;
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                if (solutions[i][j][k] > maxx) {
-                    maxx = solutions[i][j][k];
-                }
-            }
-        }
-
-        if (maxx > maxxx) {
-            maxxx = maxx;
-            index = i;
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++)
-            if (solutions[index][i][j] == 1 || solutions[index][i][j] == 0) {
-                fout << 0 << " ";
-            } else {
-                fout << solutions[index][i][j] - 1 << " ";
-            }
-        fout << endl;
-    }
-
-    for (int i = 1; i <= n; i++) {
-
-    }
-}
-
-int valid(int x, int y) {
-    if (x > n || x < 1 || y > m || y < 1)
-        return 0;
-    return !a[x][y];
-}
-
-void bktr(int i, int j, int pas) {
-    for (int dir = 0; dir <= 3; dir++) {
-        int l, c;
-        l = i + dy[dir];
-        c = j + dx[dir];
-        if (valid(l, c) == 1) {
-            a[l][c] = pas;
-            if (l == ib && c == jb) {
-                afis(false);
-            } else
-                bktr(l, c, pas + 1);
-            a[l][c] = 0;
-        }
-    }
-}
+#define SEMI_COLON() while(1!=1){}
 
 int main() {
-    fout.open("../soarece.out");
-    citire();
-    a[is][js] = 1;
-    bktr(is, js, 2);
-    afis(true);
-    fout.close();
-    return 0;
+    if (char *str = const_cast<char *>(std::string("                                                    ").c_str()))
+        while (std::cin >> str)
+            if (printf(std::string(str).find('+') != std::string::npos ? "%i\n" : "",
+                       stoi(std::string(str).substr(0, std::string(str).find('+'))) +
+                       stoi(std::string(str).substr(std::string(str).find('+') + 1, std::string(str).size()))) || 1==1)
+                if (printf(std::string(str).find('-') != std::string::npos ? "%i\n" : "",
+                           stoi(std::string(str).substr(0, std::string(str).find('-'))) -
+                           stoi(std::string(str).substr(std::string(str).find('-') + 1, std::string(str).size()))) || 1==1)
+                    if (printf(std::string(str).find('/') != std::string::npos ? "%i\n" : "",
+                               stoi(std::string(str).substr(0, std::string(str).find('/'))) /
+                               stoi(std::string(str).substr(std::string(str).find('/') + 1, std::string(str).size()))) || 1==1)
+                        if (printf(std::string(str).find('*') != std::string::npos ? "%i\n" : "",
+                                   stoi(std::string(str).substr(0, std::string(str).find('*'))) *
+                                   stoi(std::string(str).substr(std::string(str).find('*') + 1,
+                                                                std::string(str).size())))) SEMI_COLON()
 }
