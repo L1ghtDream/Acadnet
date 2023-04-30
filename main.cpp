f3e1b434f8b4458c0faee142ac8da138184f0790
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-30 23:10:03 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,90 +1,57 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
+#define INPUT_FILENAME "../input.txt"
+#define OUTPUT_FILENAME "../output.txt"
 using namespace std;
-struct Cutie {
-    int index;
-    int nr_jucarii;
-};
-struct Mutare {
-    int sursa;
-    int destinatie;
-    int jucarii;
-};
-
-bool rule(Cutie a, Cutie b) {
-    return a.nr_jucarii < b.nr_jucarii;
-}
 
 int main() {
-    int nr_cutii;
-    cin >> nr_cutii;
-    if (nr_cutii == 0 || nr_cutii == 1) {
-        cout << "0" << endl;
-    } else {
-        struct Cutie cutii[1005];
-        int medie_jucarii = 0;
-        for (int i = 0; i < nr_cutii; i++) {
-            cin >> cutii[i].nr_jucarii;
-            medie_jucarii += cutii[i].nr_jucarii;
-            cutii[i].index = i;
+    vector<vector<int>> matrix;
+    ifstream inputFile;
+    ofstream outputFile;
+    inputFile.open(INPUT_FILENAME);
+    if (!inputFile.good()) {
+        cout << "Failed to open" << INPUT_FILENAME << endl;
+        exit(-1);
+    }
+    int size;
+    inputFile >> size;
+    for (int i = 0; i < size; i++) {
+        vector<int> tmp;
+        for (int j = 0; j < size; j++) {
+            int x;
+            inputFile >> x;
+            tmp.push_back(x);
         }
-        medie_jucarii /= nr_cutii;
-        cout << "Medie: " << medie_jucarii << endl;
+        matrix.push_back(tmp);
+    }
 
-        struct Mutare mutari[1005];
-        //int count = 0;
-        //sort(cutii, cutii + nr_cutii, rule);
+    for (int i = 0; i < size; i++) {
+        sort(matrix[i].begin(), matrix[i].end());
 
-        //for (int i = 0; i < nr_cutii; i++) {
-        //    cout<<cutii[i].nr_jucarii<<" ";
-        //}
-
-        //cout<<endl;
-
-        bool moved = true;
-        int index = 0;
-
-        while (moved) {
-            moved = false;
-            int max = 0;
-            int maxIndex = -1;
-
-            int min = 100000;
-            int minIndex = -1;
-
-            for (int i = 0; i < nr_cutii; i++) {
-                if (cutii[i].nr_jucarii > max) {
-                    maxIndex = i;
-                    max = cutii[i].nr_jucarii;
-                }
-            }
-
-            for (int i = 0; i < nr_cutii; i++) {
-                if (cutii[i].nr_jucarii < min) {
-                    minIndex = i;
-                    min = cutii[i].nr_jucarii;
-                }
-            }
-
-            if (maxIndex != -1 && minIndex != -1 && max != medie_jucarii && min != medie_jucarii) {
-                moved = true;
-
-                Mutare mutare;
-                mutare.sursa = maxIndex + 1;
-                mutare.destinatie = minIndex + 1;
-                mutare.jucarii = medie_jucarii - min;
-                mutari[index++] = mutare;
-
-                cutii[minIndex].nr_jucarii = medie_jucarii;
-                cutii[maxIndex].nr_jucarii = max - medie_jucarii + min;
-            }
-        }
-
-
-        cout << index << endl;
-        for (int i = 0; i < index; i++) {
-            cout << mutari[i].sursa << " " << mutari[i].jucarii << " " << mutari[i].destinatie << endl;
+        if((i+1) % 2 == 1){
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
+
+    inputFile.close();
+
+    outputFile.open(OUTPUT_FILENAME);
+    if (!outputFile.good()) {
+        cout << "Failed to open" << OUTPUT_FILENAME << endl;
+        exit(-1);
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            outputFile << matrix[i][j] << " ";
+        }
+        outputFile << endl;
+    }
+
+    outputFile.close();
+    return 0;
 }
