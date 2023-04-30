ba39435ec910f4b94e2c36e3bb55416cf8e03649
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-30 21:03:52 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
-#include <cmath>
-#include <cstdio>
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
-using namespace std;
+bool is_prime(unsigned long long x) {
+    if (x == 2) return true;
+    for (long long i = 2; i * i <= x; i++)
+        if (x % i == 0)
+            return false;
+    return true;
+}
 
 int main() {
-    // următoarea linie oferă posibilitatea de a citi din fisierul "input.txt"
-    // folosind în continuare cin!
-    freopen("../input.txt", "r", stdin); // este corectă această linie, nu trebuiasă modifici aici!
-
-    int values;
-    cin >> values;
-
-    double x = 1.0l, limit;
-
-    // trebuie sa modifici aceasta bucla
-    while (values-- > 0) {
-        x = x / 2;
-        double half = x / 2;
-        double halfSine = sin(half);
-        limit = halfSine / half;
-        limit = limit * limit / 2;
+    unsigned long long n, count = 0, sum = 0;
+    char citire[100];
+    gets(citire);
+    n = atoi(citire);
+    for (int i = 2; i < n; i++) {
+        if (is_prime(i)){
+            count++;
+            sum += i;
+        }
     }
-    // nu modifica dupa aceasta linie
-
-    cout << x << " " << limit << "\n";
-
-    return 0;
+    std::cout << sum << " " << count << std::endl;
 }
