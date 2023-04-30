991bf40c022a85f4df42c0226bbe95d2b2d239b2
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-30 23:17:57 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,77 +1,55 @@
-#include <iostream>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sstream>
-#include <bits/stdc++.h>
+#include <assert.h>
 
-#define EXIT_CONVERTER {cout << "Invalid IPv4 address" << endl; \
-                        exit(EXIT_FAILURE);}
+#define true 1
+#define false 0
 
-using namespace std;
+#define IIF(cond) IIF_ ## cond
+#define IIF_0(t, f) f
+#define IIF_1(t, f) t
 
+#define MULTIPLY(a, b) ((a) * (b))
 
-int convert_string_to_int(string str) {
-    int x;
-    stringstream geek(str);
-    geek >> x;
-    return x;
-}
+#define ADD(a, b) ((a) + (b))
 
 
-string transform_in_binary(int octet_value) {
+int main(void) {
+    printf("Your mission, should you choose to accept it, is to test and make sure these macros work!\n");
 
-    string octet_binary;
+    printf("Expected result 1, actual result: %d\n", IIF(0)(0, 1));
+    printf("Expected result 0, actual result: %d\n", IIF(1)(0, 1));
+    printf("Expected result 0, actual result: %d\n", MULTIPLY(1, 0));
+    printf("Expected result 1, actual result: %d\n", MULTIPLY(1, 1));
+    printf("Expected result 36, actual result: %d\n", MULTIPLY(6, 6));
+    printf("Expected result 3, actual result: %d\n", MULTIPLY(1, 1 + 2));
+    printf("Expected result 3, actual result: %d\n", MULTIPLY(1 + 2, 1));
+    printf("Expected result 3, actual result: %d\n", MULTIPLY(1 + 2, 1 + 0));
+    printf("Expected result 18, actual result: %d\n", MULTIPLY(1 + 2, 6 + 0));
+    printf("Expected result 18, actual result: %d\n", MULTIPLY(1 + 2, 1 + 2) + MULTIPLY(1 + 2, 1 + 2));
 
-    for (int digit = 7; digit >= 0; digit--) {
-        octet_binary.push_back(octet_value & (1 << digit) ? '1' : '0');
-    }
+    printf("Expected result 81, actual result: %d\n", MULTIPLY(1 + 2, 1 + 2) * MULTIPLY(1 + 2, 1 + 2));
 
-    reverse(octet_binary.begin(), octet_binary.end());
+    printf("Expected result 792, actual result: %d\n", MULTIPLY(6 + 2, 1 + 2) * MULTIPLY(9 + 2, 1 + 2));
 
-    return octet_binary;
-}
+    printf("Expected result 57, actual result: %d\n", ADD(MULTIPLY(6 + 2, 1 + 2), MULTIPLY(9 + 2, 1 + 2)));
 
-string foo(string ipv4_dec) {
+    printf("Expected result -57, actual result: %d\n", -ADD(MULTIPLY(6 + 2, 1 + 2), MULTIPLY(9 + 2, 1 + 2)));
 
-    string ipv4_bin;
-    size_t pos;
-    unsigned int count = 4;
-    unsigned int octet_int;
-
-    pos = ipv4_dec.find('.');
-    while (count) {
-
-        octet_int = convert_string_to_int(ipv4_dec.substr(0, pos));
-
-        ipv4_dec = ipv4_dec.substr(pos + 1, ipv4_dec.length());
-
-        string tmp = transform_in_binary(octet_int);
-
-        reverse(tmp.begin(), tmp.end());
-
-        ipv4_bin += tmp;
-
-        if (count != 1)
-            ipv4_bin.push_back('.');
-
-        pos = ipv4_dec.find('.');
-
-        count--;
-    }
-
-    return ipv4_bin;
-}
-
-int main() {
-
-    string ipv4_dec, ipv4_bin;
-
-    cin >> ipv4_dec;
-
-    ipv4_bin = foo(ipv4_dec);
-
-    cout << ipv4_bin << endl;
+    assert(1 == IIF(0)(0, 1));
+    assert(0 == IIF(1)(0, 1));
+    assert(0 == MULTIPLY(1, 0));
+    assert(0 == MULTIPLY(0, 1));
+    assert(1 == MULTIPLY(1, 1));
+    assert(36 == MULTIPLY(6, 6));
+    assert(3 == MULTIPLY(1, 1 + 2));
+    assert(3 == MULTIPLY(1 + 2, 1));
+    assert(3 == MULTIPLY(1 + 2, 1 + 0));
+    assert(18 == MULTIPLY(1 + 2, 6 + 0));
+    assert(18 == MULTIPLY(1 + 2, 1 + 2) + MULTIPLY(1 + 2, 1 + 2));
+    assert(81 == MULTIPLY(1 + 2, 1 + 2) * MULTIPLY(1 + 2, 1 + 2));
+    assert(792 == MULTIPLY(6 + 2, 1 + 2) * MULTIPLY(9 + 2, 1 + 2));
+    assert(57 == ADD(MULTIPLY(6 + 2, 1 + 2), MULTIPLY(9 + 2, 1 + 2)));
+    assert(-57 == -ADD(MULTIPLY(6 + 2, 1 + 2), MULTIPLY(9 + 2, 1 + 2)));
 
     return 0;
 }
