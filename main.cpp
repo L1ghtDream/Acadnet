611f9afae8e7d453a99ebdc32e71fb927b893174
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-30 19:56:49 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
-#include <iostream>
+#include <cmath>
 #include <cstdio>
+#include <iostream>
+
 using namespace std;
+
 int main() {
     // următoarea linie oferă posibilitatea de a citi din fisierul "input.txt"
     // folosind în continuare cin!
-    freopen("../input.txt", "r", stdin); // este corectă această linie, nu trebuie să modifici aici!
+    freopen("../input.txt", "r", stdin); // este corectă această linie, nu trebuiasă modifici aici!
 
-    // Observati liniile 10-15. Nu trebuie să le modificati.
-    int gigel;
-    gigel = 1, 2, 3; // gigel == ?
-    ++gigel = 2;// gigel == ?
-    ++gigel = 3;// gigel == ?
+    int values;
+    cin >> values;
 
-    // Acum să terminăm cu prostiile s să îl citim pe gigel din
-    // fisier. Gigel este (un) întreg.
+    double x = 1.0l, limit;
 
-    cin >> gigel;
+    // trebuie sa modifici aceasta bucla
+    while (values-- > 0) {
+        x = x / 2;
+        double half = x / 2;
+        double halfSine = sin(half);
+        limit = halfSine / half;
+        limit = limit * limit / 2;
+    }
+    // nu modifica dupa aceasta linie
 
-    // Aveti voie să modificati linia 21.
-    int enumeration = 1;
-    ++enumeration;
+    cout << x << " " << limit << "\n";
 
-    // Aveti voie să modificati linia 25.
-    int a = gigel, b = 0, c = 3, d = 4, e = 5, f = gigel-1;
-    int foo, bar;
-    foo = ++f, e - b, c - a;
-    bar = (a + b, ++b + ++c - --a + a++, f);
-    cout << (foo + bar) << "\n";
     return 0;
 }
