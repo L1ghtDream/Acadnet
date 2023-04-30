2e78f3407dbd976adbe6ff4a4aa2ab40161f4c73
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-05-01 01:27:35 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -1,74 +1,78 @@
-#include <cstdio>
-#include <cstdlib>
-#include <vector>
+/*
+    Acadnet 2017 - Etapa Interjudeteana
+    Problema A - Optimize Nule
+*/
+
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <stdlib.h>
+
 using namespace std;
 
-void print_spiral(int m, int n, vector<vector<int> >&matrix)
-{
-    int i;
-    int x = 0, y = 0;
+#define INPUT_FILENAME "../input.txt"
+#define OUTPUT_FILENAME "../output.txt"
 
-    while (m > 0 && n > 0) {
-        if (m == 1) {
-            for (i = 0; i < n; i++) {
-                printf("%d ",matrix[x][y++]);
-            }
-            break;
+/*
+	Aceasta este singura functie pe care aveti
+	voie sa o modificati / rescrieti.
+*/
+void nule(std::vector<int> &v) {
+    vector<int> toRemove;
+    int count = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == 0) {
+            v.erase(v.begin() + i);
+            i--;
+            count++;
         }
-        else if (n == 1){
-            for (i = 0; i < m; i++){
-                printf("%d ", matrix[x++][y]);
-            }
-            break;
-        }
+    }
 
-        //go right
-        for (i = 0; i < n; i++) {
-            printf("%d ", matrix[x][y++]);
-        }
-
-        //go down
-        for (i = 0; i < m; i++) {
-            printf("%d ", matrix[x--][y]);
-        }
-
-        //go left
-        for (i = 0; i < n; i++) {
-            printf("%d ", matrix[x][y--]);
-        }
-
-        //go up
-        for (i = 0; i < m; i++) {
-            printf("%d ", matrix[x++][y]);
-        }
-
-        x++;
-        y++;
-        m--;
-        n--;
+    for(int i=0;i<count;i++){
+        v.push_back(0);
     }
 }
 
-int main()
-{
-    int n, m;
+int main(void) {
+    std::vector<int> v;
+    int size, i, x;
+    std::ifstream input_file;
+    std::ofstream output_file;
 
-    scanf("%d%d", &m, &n);
-
-    int cnt = 1;
-    int i, j;
-    vector<vector<int> > matrix = vector<vector<int> >(m, vector<int>(n, 0));
-
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++){
-            cout<<cnt++ << " ";
-        }
-        cout<<endl;
+    // Read vector from input file
+    input_file.open(INPUT_FILENAME);
+    if (!input_file.good()) {
+        std::cout << "Failed to open " << INPUT_FILENAME << std::endl;
+        exit(-1);
     }
 
-    //print_spiral(m, n, matrix);
+    input_file >> size;
 
+    v.reserve(size);
+
+    for (i = 0; i < size; i++) {
+        input_file >> x;
+        v.push_back(x);
+    }
+
+    input_file.close();
+
+
+    // Call nule() function
+    nule(v);
+
+    // Print vector to output file
+    output_file.open(OUTPUT_FILENAME);
+    if (!output_file.good()) {
+        std::cout << "Failed to open " << OUTPUT_FILENAME << std::endl;
+        exit(-1);
+    }
+
+    output_file << size << std::endl;
+    for (i = 0; i < size; i++)
+        output_file << v[i] << " ";
+
+    output_file << std::endl;
+    output_file.close();
     return 0;
 }
-
